c5dd91bef3b45857d713847f2aecbf0262610768
bekruiper <pedro.af2010@gmail.com>
2022-12-02 15:38:44 -0300

    Fibonacci v.1.0
    Added GUI, and some fixes.

--- Fibonacci_bekruiper.py
+++ Fibonacci_bekruiper.py
@@ -1,15 +1,48 @@
-n = 10000   
+import PySimpleGUI as sg
+import re
+# Window
+layout = [[sg.Text("Welcome to Fibonacci Basic Generator")],
+          [sg.Text("Please introduce a number to generate sequence")],
+          [sg.Input(key='-INPUT-')],
+          [sg.Button('Generate', key='-GENERATE-'), sg.Exit()]]
+window = sg.Window('Fibonacci Basic Generator', layout)
+event, values = window.read()
+
+
+# Function
+
 
 def fibonacci_gen(n) -> int:
     yield 0
     prev2 = 0
     prev1 = 1
-    while prev1 <= n:
+    result_list = []
+    for i in range(n):
         result = prev1+prev2
         prev2 = prev1
         prev1 = result
+        result = re.sub("(\d)(?=(\d{3})+(?!\d))", r"\1,", "%d" % result)
         yield result
 
-for i in fibonacci_gen(n) :
-    print(i)
-    
+
+# Trigger
+while True:
+    event, values = window.read()
+    try:
+        n = int(values['-INPUT-'])
+    except:
+        pass
+
+    # Result
+    if event == '-GENERATE-':
+        for i in fibonacci_gen(n):
+            result_list = list(fibonacci_gen(n))
+        str_result_list = ""
+        for i in result_list:
+            str_result_list += "{}{}".format(i, " / ")
+        new_list = str_result_list[:-len("/")]
+        sg.popup('Your Sequence', str_result_list)
+
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        window.close()
+        break
